2d35aa995ad0d4f85678a77d4dfb33b645d4e283
Matt Capone <matcpn@gmail.com>
2017-05-18 18:47:43 -0400

    changed everything, added a ton of features, everything is much more abstract

--- mysite/housing/views.py
+++ mysite/housing/views.py
@@ -4,11 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render, render_to_response, reverse, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from models import Lot, House, Kitchen, Bathroom, PricePerSquareFootUpgrade, UserChoice, Bedroom, LivingRoom, DiningRoom, Garage, UserRoomUpgradeMapping, FlatPriceUpgrade
+from models import Lot, House, Kitchen, Bathroom, PricePerSquareFootUpgrade, UserChoice, Bedroom, LivingRoom, DiningRoom, Garage, UserRoomUpgradeMapping, FlatPriceUpgrade, Room
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+
+room_types_array = ["Kitchen", "Bathroom", "Dining Room", "Living Room", "Garage", "Bedroom"]
 
 # Create your views here.
 def login(request):
@@ -74,86 +76,75 @@ def house(request):
 	return render(request, 'houses.html', context)
 
 @login_required
-def features(request, context):
+def room_types(request):
 	request.session['total_price'] = int(calcTotalPrice(request))
-	return render(request, 'features.html', context)
+	context = { 'room_types' : room_types_array }
+	return render(request, 'room_types.html', context)
+
+@login_required
+def select_room_type(request):
+	user_choice = get_user_choice_for_user(request.user.username)
+	room_type = request.POST['room_type']
+	if room_type in 'Kitchen':
+		context = { 'rooms' : user_choice.house.kitchen.all() }
+	if room_type == 'Bathroom':
+		context = { 'rooms' : user_choice.house.bathroom.all() }
+	if room_type == 'Bedroom':
+		context = { 'rooms' : user_choice.house.bedroom.all() }
+	if room_type == 'Garage':
+		context = { 'rooms' : user_choice.house.garage.all() }
+	if room_type == 'Living Room': 
+		context = { 'rooms' : user_choice.house.livingRoom.all() }
+	if room_type == 'Dining Room':
+		context = { 'rooms' : user_choice.house.diningRoom.all() }
+	context['roomname'] = room_type
+	return render(request, 'rooms.html', context)
 
 @login_required
 def select_feature(request):
 	user_choice = get_user_choice_for_user(request.user.username)
-	feature = request.POST['feature']
-	if 'kitchen' in feature:
-		kitchen_id = feature.split(",")[1]
-		context = {
-			'upgrades' : user_choice.house.kitchen.get(pk=kitchen_id).ppsf_upgrades.all(),
-			'flat_price_upgrades' : user_choice.house.kitchen.get(pk=kitchen_id).flat_price_upgrades.all(),
-			'kitchen' : kitchen_id
-		}
-		return render(request, 'kitchen.html', context)
-	if 'bathroom' in feature:
-		bathroom_id = feature.split(",")[1]
-		context = {
-			'upgrades' : Bathroom.objects.get(pk=bathroom_id).ppsf_upgrades.all(),
-			'flat_price_upgrades' : user_choice.house.bathroom.get(pk=bathroom_id).flat_price_upgrades.all(),
-			'bathroom' : bathroom_id
-		}
-		return render(request, 'bathroom.html', context)
+	room = request.POST['room']
+	room_info = room.split(",")
+	room_type = room_info[0]
+	room_id = room_info[1]
+	room_chosen = get_all_apgrades_for_chosen_room(room_type, room_id, user_choice)
+	context = {
+		'upgrades' : room_chosen.ppsf_upgrades.all(),
+		'flat_price_upgrades' : room_chosen.flat_price_upgrades.all(),
+		'room' : room_id,
+		'room_type' : room_type
+	}
+	return render(request, 'upgrades.html', context)
 
 @login_required
-def select_kitchen_upgrade(request):
+def select_room_upgrade(request):
 	user_choice = get_user_choice_for_user(request.user.username)
 	request_variables = request.POST['upgrade'].split(",")
 	print(request_variables)
 	chosen_upgrade_id = request_variables[0]
-	kitchen_id = request_variables[1]
-	kitchen = Kitchen.objects.get(pk=kitchen_id)
+	room_id = request_variables[1]
+	room_type = request_variables[3]
+	room_object = get_object_for_room_type(room_type, room_id)
 	is_ppsf_upgrade = request_variables[2]
 
 	#create a new mapping for the new upgrade chosen
 	if is_ppsf_upgrade == 'True':
 		chosen_upgrade = PricePerSquareFootUpgrade.objects.get(pk=chosen_upgrade_id)
-		new_mapping = UserRoomUpgradeMapping(user=user_choice, room=kitchen, ppsf_upgrade=chosen_upgrade, upgrade_type=chosen_upgrade.upgrade_type, roomname=kitchen.name)
+		new_mapping = UserRoomUpgradeMapping(user=user_choice, room=room_object, ppsf_upgrade=chosen_upgrade, upgrade_type=chosen_upgrade.upgrade_type, roomname=room_object.name)
 	else:
 		chosen_upgrade = FlatPriceUpgrade.objects.get(pk=chosen_upgrade_id)
-		new_mapping = UserRoomUpgradeMapping(user=user_choice, room=kitchen, flat_price_upgrade=chosen_upgrade, upgrade_type=chosen_upgrade.upgrade_type, roomname=kitchen.name)
+		new_mapping = UserRoomUpgradeMapping(user=user_choice, room=room_object, flat_price_upgrade=chosen_upgrade, upgrade_type=chosen_upgrade.upgrade_type, roomname=room_object.name)
 	
 	#check if old upgrade already had a mapping, if so, delete it
-	same_type_upgrade = UserRoomUpgradeMapping.objects.get(user=user_choice, room=kitchen, upgrade_type=chosen_upgrade.upgrade_type)
-	if same_type_upgrade is not None:
-		same_type_upgrade.delete()
-
-	new_mapping.save()
-	context = get_context_for_user_choice(request)
-	return features(request, context)
-
-@login_required
-def select_bathroom_upgrade(request):
-	user_choice = get_user_choice_for_user(request.user.username)
-	request_variables = request.POST['upgrade'].split(",")
-	chosen_upgrade_id = request_variables[0]
-	bathroom_id = request_variables[1]
-	bathroom = Bathroom.objects.get(pk=bathroom_id)
-	is_ppsf_upgrade = request_variables[2]
-
-	#create a new mapping for the new upgrade chosen
-	if is_ppsf_upgrade == 'True':
-		chosen_upgrade = PricePerSquareFootUpgrade.objects.get(pk=chosen_upgrade_id)
-		new_mapping = UserRoomUpgradeMapping(user=user_choice, room=bathroom, ppsf_upgrade=chosen_upgrade, upgrade_type=chosen_upgrade.upgrade_type, roomname=bathroom.name)
-	else:
-		chosen_upgrade = FlatPriceUpgrade.objects.get(pk=chosen_upgrade_id)
-		new_mapping = UserRoomUpgradeMapping(user=user_choice, room=bathroom, flat_price_upgrade=chosen_upgrade, upgrade_type=chosen_upgrade.upgrade_type, roomname=bathroom.name)
-
-	#check if old upgrade already had a mapping, if so, delete it
 	try:
-		same_type_upgrade = UserRoomUpgradeMapping.objects.filter(user=user_choice, room=bathroom, upgrade_type=chosen_upgrade.upgrade_type)
+		same_type_upgrade = UserRoomUpgradeMapping.objects.filter(user=user_choice, room=room_object, upgrade_type=chosen_upgrade.upgrade_type)
 		if same_type_upgrade.exists():
 			same_type_upgrade.delete()
 	except ObjectDoesNotExist:
 		print("no objects found")
 
 	new_mapping.save()
-	context = get_context_for_user_choice(request)
-	return features(request, context)
+	return room_types(request)
 
 @login_required
 def calcTotalPrice(request):
@@ -165,8 +156,7 @@ def select_house(request):
 	user_choice = get_user_choice_for_user(request.user.username)
 	user_choice.house = House.objects.get(pk=request.POST['house_id'])
 	user_choice.save()
-	context = get_context_for_user_choice(request)
-	return features(request, context)
+	return room_types(request)
 
 def get_user_choice_for_user(username):
 	user = User.objects.get(username=username)
@@ -187,3 +177,31 @@ def get_context_for_user_choice(request):
 		'dining_rooms' : user_choice.house.diningRoom.all(),
 		}
 	return context
+
+def get_object_for_room_type(room_type, room_id):
+	if room_type == "Kitchen":
+		return Kitchen.objects.get(pk=room_id)
+	if room_type == "Bathroom":
+		return Bathroom.objects.get(pk=room_id)
+	if room_type == "LivingRoom":
+		return LivingRoom.objects.get(pk=room_id)
+	if room_type == "Garage":
+		return Garage.objects.get(pk=room_id)
+	if room_type == "DiningRoom":
+		return DiningRoom.objects.get(pk=room_id)
+	if room_type == "Bedroom":
+		return Bedroom.objects.get(pk=room_id)
+
+def get_all_apgrades_for_chosen_room(room_type, room_id, user_choice):
+	if room_type == "Kitchen":
+		return user_choice.house.kitchen.get(pk=room_id)
+	if room_type == "Bathroom":
+		return user_choice.house.bathroom.get(pk=room_id)
+	if room_type == "LivingRoom":
+		return user_choice.house.livingRoom.get(pk=room_id)
+	if room_type == "Garage":
+		return user_choice.house.garage.get(pk=room_id)
+	if room_type == "DiningRoom":
+		return user_choice.house.diningRoom.get(pk=room_id)
+	if room_type == "Bedroom":
+		return user_choice.house.bedroom.get(pk=room_id)
